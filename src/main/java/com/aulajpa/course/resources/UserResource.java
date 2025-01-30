a33cd03db74cbcaac9f7bde806d87586ee56b45d
Everton Vitor da Silva <everton.vitordasilva@outlook.com>
2025-01-30 19:42:52 -0300

    Inclusion of classes User and UserResources

---
+++ src/main/java/com/aulajpa/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.aulajpa.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aulajpa.course.entities.User;
+
+@RestController
+@RequestMapping(value= "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "11934276543", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
