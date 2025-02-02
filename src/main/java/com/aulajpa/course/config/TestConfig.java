f70530bf99bb8cef9c62c5c0fcbbb101cbadf1b9
Everton Vitor da Silva <everton.vitordasilva@outlook.com>
2025-02-01 21:01:34 -0300

    Include Enum OrderStatus

--- src/main/java/com/aulajpa/course/config/TestConfig.java
+++ src/main/java/com/aulajpa/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.aulajpa.course.entities.Order;
 import com.aulajpa.course.entities.User;
+import com.aulajpa.course.entities.enums.OrderStatus;
 import com.aulajpa.course.repositories.OrderRepository;
 import com.aulajpa.course.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2025-02-01T13:57:30Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2025-02-01T13:59:45Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2025-02-01T14:05:10Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2025-02-01T13:57:30Z"), OrderStatus.DELIVERED ,u1);
+		Order o2 = new Order(null, Instant.parse("2025-02-01T13:59:45Z"), OrderStatus.WATING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2025-02-01T14:05:10Z"),OrderStatus.CANCELED ,u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
