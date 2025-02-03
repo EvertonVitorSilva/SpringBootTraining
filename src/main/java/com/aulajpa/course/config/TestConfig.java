5b2b12206d5c5b77059895f863e9a1ec712c182b
Everton Vitor da Silva <everton.vitordasilva@outlook.com>
2025-02-02 21:22:00 -0300

    Inclusion of class Payment and your associations

--- src/main/java/com/aulajpa/course/config/TestConfig.java
+++ src/main/java/com/aulajpa/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.aulajpa.course.entities.Category;
 import com.aulajpa.course.entities.Order;
 import com.aulajpa.course.entities.OrderItem;
+import com.aulajpa.course.entities.Payment;
 import com.aulajpa.course.entities.Product;
 import com.aulajpa.course.entities.User;
 import com.aulajpa.course.entities.enums.OrderStatus;
@@ -46,7 +47,7 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2025-02-01T13:57:30Z"), OrderStatus.DELIVERED ,u1);
+		Order o1 = new Order(null, Instant.parse("2025-02-01T13:57:30Z"), OrderStatus.PAID ,u1);
 		Order o2 = new Order(null, Instant.parse("2025-02-01T13:59:45Z"), OrderStatus.WATING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2025-02-01T14:05:10Z"),OrderStatus.CANCELED ,u1);
 		
@@ -84,6 +85,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2025-02-01T17:30:17Z"), o1);
+		
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 	
