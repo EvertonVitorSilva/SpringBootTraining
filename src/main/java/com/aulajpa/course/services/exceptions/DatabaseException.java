7bf8c99aa3c22b7c016c2035f4dbaedc253884a9
Everton Vitor da Silva <everton.vitordasilva@outlook.com>
2025-02-04 23:05:39 -0300

    Include exceptions handler for findById, delete and update

---
+++ src/main/java/com/aulajpa/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,12 @@
+package com.aulajpa.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+	static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+ 
+	
+	
+}
